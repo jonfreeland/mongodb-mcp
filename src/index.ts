d298c9c322eb28c05083650b5fdcce841726cfd1
Nathan Pahucki <npahucki@gmail.com>
2025-04-05 09:07:02 -0300

    Fix documentation around $addFields.

--- src/index.ts
+++ src/index.ts
@@ -466,13 +466,14 @@ Supported Stages:
 - $group: Group documents by a key
 - $sort: Sort documents
 - $project: Shape the output
+- $addFields - Include additional or calculated fields
 - $lookup: Perform left outer joins
 - $unwind: Deconstruct array fields
+
 
 Unsafe/Blocked Stages:
 - $out: Write results to collection
 - $merge: Merge results into collection
-- $addFields: Add new fields
 - $set: Set field values
 - $unset: Remove fields
 - $replaceRoot: Replace document structure
